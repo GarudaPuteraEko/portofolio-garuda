ef7f4726e31cfcee78deeace344b45885ab30117
GarudaPuteraEko <garudaputera57@gmail.com>
2024-10-24 09:09:53 +0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,66 @@
+import project1 from "../assets/projects/knightGame.webp";
+import project2 from "../assets/projects/todolist.webp";
+import project3 from "../assets/projects/rudaOlshop.webp";
+import project4 from "../assets/projects/imt.webp";
+
+export const HERO_CONTENT = `I am a passionate Game Developer and Web Developer with a strong focus on creating interactive, user-friendly experiences. With expertise in both game design and full-stack web development, I bring creative ideas to life through immersive gameplay and efficient, visually appealing websites. I thrive on solving complex problems and continuously expanding my knowledge to stay ahead of the latest trends in technology. My work is driven by a commitment to quality, innovation, and delivering products that inspire and engage users.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2020 - Present",
+    role: "Youtuber",
+    company: "Google Inc.",
+    description: `Create Video Game content for many people.`,
+    technologies: ["XRecorder", "Capcut", "Pixelab"],
+  },
+  {
+    year: "2023 - Present",
+    role: "Software Engineering Student",
+    company: "SMKN 21 Jakarta",
+    description: `Learn anything about software engineer.`,
+    technologies: ["HTML", "CSS", "JavaScript", "mySQL", "PHP", "Java", "C", "C++", "C#", "AndroidStudio", "Unity", "GDevelop", "Laravel", "NextJs"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Knight Adventure Game",
+    image: project1,
+    url: "https://ruda102.itch.io/short-adventure-of-knight",
+    description:
+      "A platformer game that can fill you free time.",
+    technologies: ["GDevelop5", "Java"],
+  },
+  {
+    title: "To Do List Website",
+    image: project2,
+    url: "https://github.com/GarudaPuteraEko/ToDo-List",
+    description:
+      "A website to help you plan something.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Online Shop Website",
+    image: project3,
+    url: "https://github.com/GarudaPuteraEko/olshop_ruda",
+    description:
+      "A website to make it easier for you to buy a motorbike.",
+    technologies: ["HTML", "CSS", "PHP", "MySQL"],
+  },
+  {
+    title: "IMT Website",
+    image: project4,
+    url: "https://github.com/GarudaPuteraEko/tugas_imt",
+    description:
+      "A website to measure your body mass index (BMI).",
+    technologies: ["HTML", "CSS", "PHP"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kemayoran, jakarta Pusat",
+  phoneNo: "+62 859-2076-5720",
+  email: "garudaputera57@gmail.com",
+};
